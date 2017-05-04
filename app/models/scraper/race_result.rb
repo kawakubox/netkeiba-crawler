77c7fe51a88cc23cbe3e245d530478b987065a25
kawakubox <shigefumi.kawakubo@gmail.com>
2017-05-05 06:43:31 +0900

    馬柱の過去成績セルの scraper (#10)
    * Add: race result cell scraper
    
    * Add: race_time scraper
    
    * Add: horse_weight, weight_diff scraper
    
    * Add: gate_number, horse_number, popularity
    
    * Add: jockey_key, jockey_weight scraper

---
+++ app/models/scraper/race_result.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Scraper
+  class RaceResult
+    def initialize(html)
+      @doc = Nokogiri::HTML(html)
+    end
+
+    def race
+      Race.find_or_create_by!(key: race_key)
+    end
+
+    def race_key
+      @doc.at('div a').attr('href').match(%r{/race/result/(\d+)/})[1]
+    end
+
+    def course_condition
+      @doc.at('div:nth(1)').attr('class').match(/i(\d{2})(\d{2})/)[1]
+    end
+
+    def order
+      @doc.at('div:nth(1)').attr('class').match(/i(\d{2})(\d{2})/)[2].to_i
+    end
+
+    def race_time
+      md = @doc.at('td > strong').text.match(/(\d+)?.(\d+).(\d+)/)
+      md[1].to_i * 60 + md[2].to_i + md[3].to_i * 0.1
+    end
+
+    def jockey_key
+      @doc.at('td > a').attr('href').match(%r{/directory/jocky/(\d+)/})[1]
+    end
+
+    def jockey_weight
+      @doc.at('td > a').next_sibling.text.match(/\((\d+)\)/)[1].to_i
+    end
+
+    def horse_weight
+      @doc.text.match(/(\d{3})\(([+-]?\d+)\)/)[1].to_i
+    end
+
+    def weight_diff
+      @doc.text.match(/(\d{3})\(([+-]?\d+)\)/)[2].to_i
+    end
+
+    def gate_number
+      @doc.text.match(/\[(\d+)\](\d+)\((\d+)人\)/)[1].to_i
+    end
+
+    def horse_number
+      @doc.text.match(/\[(\d+)\](\d+)\((\d+)人\)/)[2].to_i
+    end
+
+    def popularity
+      @doc.text.match(/\[(\d+)\](\d+)\((\d+)人\)/)[3].to_i
+    end
+  end
+end
---
+++ spec/models/scraper/race_result_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Scraper::RaceResult do
+  let(:html) { File.read(File.join(Rails.root, 'spec/fixtures/html/yahoo_race_result_cell.html')) }
+  subject { Scraper::RaceResult.new(html) }
+
+  its(:race_key) { is_expected.to eq '1505021210' }
+  its(:course_condition) { is_expected.to eq '01' }
+  its(:order) { is_expected.to eq 1 }
+  its(:race_time) { is_expected.to eq 143.2 }
+  its(:jockey_key) { is_expected.to eq '05212' }
+  its(:jockey_weight) { is_expected.to eq 57 }
+  its(:horse_weight) { is_expected.to eq 484 }
+  its(:weight_diff) { is_expected.to eq -2 }
+  its(:gate_number) { is_expected.to eq 7 }
+  its(:horse_number) { is_expected.to eq 14 }
+  its(:popularity) { is_expected.to eq 1 }
+end
