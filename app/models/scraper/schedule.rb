2627841787df4a816cd095885ee0572781104ed4
kawakubox <shigefumi.kawakubo@gmail.com>
2017-05-13 20:07:06 +0900

    Change parameter

--- app/models/scraper/schedule.rb
+++ app/models/scraper/schedule.rb
@@ -10,7 +10,7 @@ module Scraper
     def scrape
       @doc.search('table.scheLs tr:has(td[rowspan] a)').map do |e|
         ec = EventComponent.new(e, year: year, month: month)
-        Event.new(key: ec.key, date: ec.date, name: ec.name)
+        Event.new(key: ec.key, held_on: ec.date, name: ec.name)
       end
     end
 
