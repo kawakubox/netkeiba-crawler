6e5260c32878a32417d7a36f8e2ebbcd4e195238
kawakubox <shigefumi.kawakubo@gmail.com>
2017-05-13 18:49:00 +0900

    Change event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class Event < ApplicationRecord
-  validates :key,  presence: true
-  validates :date, presence: true
-  validates :name, presence: true
+  validates :key,     presence: true, uniqueness: true
+  validates :held_on, presence: true
+  validates :name,    presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  let(:event) { Event.new(key: '17060101', date: Time.zone.today, name: '1回中山1日') }
-  it { expect(event).to be_valid }
-  it { expect(event).to be_invalid_on(:key).with(nil) }
-  it { expect(event).to be_invalid_on(:date).with(nil) }
-  it { expect(event).to be_invalid_on(:name).with(nil) }
+  subject(:event) { Event.new(key: '17060101', held_on: Time.zone.today, name: '1回中山1日') }
+  it { is_expected.to be_valid }
+  it { is_expected.to be_invalid_on(:key).with(nil) }
+  specify do
+    Event.create(key: '17060101', held_on: Time.zone.today, name: '1回中山1日')
+    expect(event.invalid?).to be_truthy
+  end
+  it { is_expected.to be_invalid_on(:held_on).with(nil) }
+  it { is_expected.to be_invalid_on(:name).with(nil) }
 end
