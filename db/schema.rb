4edec41036a2e2b9bc7391a71786c784bea4cc08
kawakubox <shigefumi.kawakubo@gmail.com>
2017-05-30 00:58:16 +0900

    Update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -1,25 +1,22 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20170505123015) do
+ActiveRecord::Schema.define(version: 20170507000000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "events", force: :cascade do |t|
+    t.string   "key"
+    t.date     "held_on"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_events_on_key", unique: true, using: :btree
+  end
+
   create_table "horse_results", force: :cascade do |t|
-    t.integer  "race_id"
-    t.integer  "horse_id"
+    t.integer  "horse_id",                                 null: false
+    t.integer  "race_id",                                  null: false
     t.integer  "jockey_id"
-    t.integer  "trainer"
+    t.integer  "trainer_id"
     t.integer  "order"
     t.decimal  "race_time",        precision: 4, scale: 1
     t.decimal  "jockey_weight",    precision: 3, scale: 1
@@ -33,26 +30,31 @@ ActiveRecord::Schema.define(version: 20170505123015) do
     t.string   "corner_position"
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
+    t.index ["horse_id", "race_id"], name: "index_horse_results_on_horse_id_and_race_id", unique: true, using: :btree
+    t.index ["jockey_id"], name: "index_horse_results_on_jockey_id", using: :btree
+    t.index ["race_id"], name: "index_horse_results_on_race_id", using: :btree
+    t.index ["trainer_id"], name: "index_horse_results_on_trainer_id", using: :btree
   end
 
   create_table "horses", force: :cascade do |t|
-    t.string   "key"
+    t.string   "key",        null: false
     t.string   "name"
     t.date     "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["key"], name: "index_horses_on_key", using: :btree
+    t.index ["key"], name: "index_horses_on_key", unique: true, using: :btree
   end
 
   create_table "jockeys", force: :cascade do |t|
-    t.string   "key"
+    t.string   "key",        null: false
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_jockeys_on_key", unique: true, using: :btree
   end
 
   create_table "races", force: :cascade do |t|
-    t.string   "key"
+    t.string   "key",              null: false
     t.integer  "ordinal"
     t.string   "name"
     t.integer  "grade"
@@ -61,15 +63,15 @@ ActiveRecord::Schema.define(version: 20170505123015) do
     t.integer  "course_condition"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.index ["key"], name: "index_races_on_key", using: :btree
+    t.index ["key"], name: "index_races_on_key", unique: true, using: :btree
   end
 
   create_table "trainers", force: :cascade do |t|
-    t.string   "key"
+    t.string   "key",        null: false
     t.string   "name"
     t.integer  "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_trainers_on_key", unique: true, using: :btree
   end
-
 end
