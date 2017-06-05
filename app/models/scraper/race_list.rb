1d73572c059f5a21f941c30bf8cfe5e84c6de0ef
kawakubox <shigefumi.kawakubo@gmail.com>
2017-06-05 23:31:26 +0900

    Fix: race list scraper

--- app/models/scraper/race_list.rb
+++ app/models/scraper/race_list.rb
@@ -8,7 +8,7 @@ module Scraper
     end
 
     def scrape
-      @doc.search('table.scheLs tr[has(td:nth(2))]')[1..-1].map do |tr|
+      @doc.search('table.scheLs tr[has(td:nth(2))]').map do |tr|
         e = tr.at('td a')
         Race.find_or_initialize_by(
           key: e.attr('href').match(%r{/race/result/(\d{10})/})[1],
