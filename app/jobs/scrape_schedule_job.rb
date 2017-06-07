ede54bf0e2519cb35d821b945a3266e1b0cf895b
kawakubox <shigefumi.kawakubo@gmail.com>
2017-06-08 01:30:06 +0900

    Fix: scrape url

--- app/jobs/scrape_schedule_job.rb
+++ app/jobs/scrape_schedule_job.rb
@@ -6,7 +6,7 @@ class ScrapeScheduleJob < ApplicationJob
   queue_as :default
 
   def perform(year, month)
-    url = "https://keiba.yahoo.co.jp/schedule/list/#{year}?month=#{month}"
+    url = "https://keiba.yahoo.co.jp/schedule/list/#{year}/?month=#{month}"
     scraper = Scraper::Schedule.new(Faraday.get(url).body)
     scraper.scrape!
   end
