84dd6b857aeffd751dc04adcd4aa7d5fba68ab06
kawakubox <shigefumi.kawakubo@gmail.com>
2017-06-26 22:26:34 +0900

    Refactor: Use RaceMeta scraper

--- app/models/scraper/race_entry.rb
+++ app/models/scraper/race_entry.rb
@@ -6,17 +6,20 @@ module Scraper
       @race = Race.find_or_create_by!(key: race_key)
       response = Faraday.get(@race.yahoo_race_entry_url)
       raise StandardError unless response.success?
+      @html = response.body
       @doc = Nokogiri::HTML(response.body)
     end
 
     def scrape!
+      race_meta = Scraper::RaceMetaCell.new(@html)
+
       @race.update!(
-        ordinal:        ordinal,
-        name:           name,
-        grade:          grade,
-        distance:       distance,
-        weather:        weather,
-        course_condition: course_condition
+        ordinal:          race_meta.ordinal,
+        name:             race_meta.name,
+        grade:            race_meta.grade,
+        distance:         race_meta.distance,
+        weather:          race_meta.weather,
+        course_condition: race_meta.course_condition
       )
 
       entry_table.search('tr')[1..-1].each do |tr|
@@ -66,22 +69,6 @@ module Scraper
       end
     end
 
-    def race_key
-      @doc.at('#raceNoNaviC a').attr('href').split('/').last
-    end
-
-    def race
-      race = Race.find_or_initialize_by(key: race_key)
-      race.update(
-        ordinal: ordinal,
-        name: name,
-        grade: grade,
-        distance: distance,
-        weather: weather
-      )
-      race
-    end
-
     def jockey(tr)
       element = tr.at('td:nth(4) > a')
       key = element.attr('href').match(%r{/directory/jocky/(\d+)/})[1]
@@ -89,45 +76,6 @@ module Scraper
       Jockey.find_or_initialize_by(key: key) do |j|
         j.name = name
       end
-    end
-
-    def ordinal
-      md = @doc.at('#raceTitName h1').text.match(/第(\d+)回/)
-      md[1].to_i if md
-    end
-
-    def name
-      md = @doc.at('#raceTitName h1').text.match(/(第\d+回)?(.+)/)
-      md[2].gsub(/（.+）/, '') if md
-    end
-
-    def grade
-      md = @doc.at('#raceTitName h1').text.match(/（(.+)）/)
-      return unless md
-      case md[1]
-      when 'GI' then :g1
-      when 'GII' then :g2
-      when 'GIII' then :g3
-      end
-    end
-
-    def distance
-      md = @doc.at('#raceTitMeta').text.match(/(\d+)m/)
-      md[1].to_i if md
-    end
-
-    def weather
-      return :sunny if @doc.at('img.hare')
-      return :cloudy if @doc.at('img.kumori')
-      return :rainy if @doc.at('img.ame')
-      return :snowy if @doc.at('img.yuki')
-    end
-
-    def course_condition
-      return :good if @doc.at('img.ryou')
-      return :good_to_soft if @doc.at('img.yayaomo')
-      return :soft if @doc.at('img.omo')
-      return :heavy if @doc.at('img.furyou')
     end
   end
 end
