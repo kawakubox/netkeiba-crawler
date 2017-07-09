3181fa57a67bc835675951de0afbc35f9ebf3f78
kawakubox <shigefumi.kawakubo@gmail.com>
2017-07-09 18:01:55 +0900

    Change: optional true

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,9 +3,9 @@
 class Horse < ApplicationRecord
   validates :key, presence: true, uniqueness: true, format: /\A[a-z0-9]{10}\Z/
 
-  belongs_to :sire,    foreign_key: :sire_id, class_name: :Horse
-  belongs_to :mare,    foreign_key: :mare_id, class_name: :Horse
-  belongs_to :trainer
+  belongs_to :sire,        optional: true, foreign_key: :sire_id, class_name: :Horse
+  belongs_to :mare,        optional: true, foreign_key: :mare_id, class_name: :Horse
+  belongs_to :trainer,     optional: true
   has_many :horse_results
 
   def latest_results(date, limit = 5)
