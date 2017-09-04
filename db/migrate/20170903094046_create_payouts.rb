fb84cda1398e37e4a44cf4001681355b2d758536
kawakubox <shigefumi.kawakubo@gmail.com>
2017-09-04 23:00:35 +0900

    Implements migration file

--- db/migrate/20170903094046_create_payouts.rb
+++ db/migrate/20170903094046_create_payouts.rb
@@ -1,8 +1,16 @@
 class CreatePayouts < ActiveRecord::Migration[5.0]
   def change
     create_table :payouts do |t|
-
+      t.integer :race_id,    null: false
+      t.integer :bet_type,   null: false
+      t.integer :number_1
+      t.integer :number_2
+      t.integer :number_3
+      t.integer :payout,     null: false
+      t.integer :popularity, null: false
       t.timestamps
+
+      t.index :race_id
     end
   end
 end
