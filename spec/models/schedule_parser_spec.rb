886e2829fce78985ddb4c7dd4ea886656ef641af
kawakubox <shigefumi.kawakubo@gmail.com>
2017-09-16 13:42:25 +0900

    Refactored

--- spec/models/schedule_parser_spec.rb
+++ spec/models/schedule_parser_spec.rb
@@ -2,25 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe Scraper::Schedule do
+RSpec.describe ScheduleParser do
   let(:html) { File.read(File.join(Rails.root, 'spec/fixtures/html/yahoo_schedule.html')) }
-  subject { Scraper::Schedule.new(html) }
+  subject { ScheduleParser.new(html) }
 
-  its('scrape.size') { is_expected.to eq 24 }
+  its('events.size') { is_expected.to eq 24 }
   context 'when shedule page uncompleted' do
     let(:html) { File.read(File.join(Rails.root, 'spec/fixtures/html/yahoo_schedule_uncomplete.html')) }
-    its('scrape.size') { is_expected.to eq 22 }
+    its('events.size') { is_expected.to eq 22 }
   end
 
-  its(:scrape) { is_expected.to all(be_a(Event)) }
-  its('scrape.first') do
+  its(:events) { is_expected.to all(be_a(Event)) }
+  its('events.first') do
     is_expected.to have_attributes(
       key:     '17060101',
       held_on: Date.new(2017, 1, 5),
       name:    '1回中山1日'
     )
   end
-  its('scrape.last') do
+
+  its('events.last') do
     is_expected.to have_attributes(
       key:     '17080202',
       held_on: Date.new(2017, 1, 29),
