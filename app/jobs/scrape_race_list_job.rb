95c8e2e69bb9ef54e9bea0e63f2dd2b7aa9cf3d2
kawakubox <shigefumi.kawakubo@gmail.com>
2017-09-24 05:12:33 +0900

    Change parameter

--- app/jobs/scrape_race_list_job.rb
+++ app/jobs/scrape_race_list_job.rb
@@ -5,8 +5,9 @@ class ScrapeRaceListJob < ApplicationJob
 
   rescue_from(StandardError) { |e| Raven.capture_exception(e, extra: arguments.first) }
 
-  # @param [Event] event
-  def perform(event)
+  # @param [String] event_id
+  def perform(event_id)
+    event = Event.find(event_id)
     res = Faraday.get(event.url)
     raise "#{res.status} : #{res.reason_phrase}" unless res.success?
     RaceListParser.new(res.body).races.map(&:save!)
